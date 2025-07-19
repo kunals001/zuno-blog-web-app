5f86e57e71e7f99d0e94263186dfbcde751405d2
kunals001 <kunaltrad555@gmail.com>
2025-07-19 12:28:01 +0530

    client setup complete

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Barlow,Poppins } from "next/font/google";
+import "./globals.css";
+
+const barlow = Barlow({
+  variable: "--font-barlow",
+  subsets: ["latin"],
+  weight:["500","600","700"]
+});
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight:["400","500","600","700"]
+});
+
+export const metadata: Metadata = {
+  title: "Zuno",
+  description: "Zuno is a social blogging platform to share your thoughts and connect with others.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${barlow.variable} ${poppins.variable} antialiased`}
+        cz-shortcut-listen="true"
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
