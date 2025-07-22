2f2ae8ee9c19672af6ce2ea6354bcb0379f871f1
kunals001 <kunaltrad555@gmail.com>
2025-07-22 20:54:18 +0530

    add profile dropdown

--- client/src/components/Layouts/NavUserShow.tsx
+++ client/src/components/Layouts/NavUserShow.tsx
@@ -1,25 +1,104 @@
-import React from "react";
-import { IconUser } from "@tabler/icons-react";
-import { useAppSelector } from "@/redux/hooks";
+import React, { useState, useRef, InputHTMLAttributes } from "react";
+import {
+  IconLogout,
+  IconSettings,
+  IconUser,
+  IconUserStar,
+  IconWriting,
+} from "@tabler/icons-react";
+import { useAppSelector, useAppDispatch } from "@/redux/hooks";
+import { logoutUser } from "@/redux/slices/userSlice";
+import { useClickOutside } from "./ClickOutside";
 import Image from "next/image";
 import Link from "next/link";
 
 const NavUserShow = () => {
   const { user } = useAppSelector((state) => state.user);
+  const [openPanel, setOpenPanel] = useState(false);
+  const profileRef = useRef<HTMLDivElement>(null);
+  const panelRef = useRef<HTMLDivElement>(null);
+
+  useClickOutside({
+    ref: panelRef,
+    onOutsideClick: () => setOpenPanel(false),
+    ignoreRefs: [profileRef],
+  });
+
+  const dispatch = useAppDispatch();
 
   const isUserLoggedIn = user && user.isVerified;
+
+  const handelLogout = () => {
+    dispatch(logoutUser()).unwrap();
+    window.location.href = "/";
+  };
+
+  const PanelLinks: {
+    name: string;
+    path: string;
+    icon: React.ComponentType<{ className?: string }>;
+  }[] = [
+    {
+      name: "Profile",
+      path: "/profile",
+      icon: IconUserStar,
+    },
+    {
+      name: "Write",
+      path: "/write",
+      icon: IconWriting,
+    },
+    {
+      name: "Settings",
+      path: "/settings",
+      icon: IconSettings,
+    },
+  ];
 
   return (
     <div aria-label="show user">
       {isUserLoggedIn ? (
-        <div className="md:w-[2.5vw] md:h-[2.5vw] h-[4.7vh] w-[4.7vh] rounded-full overflow-hidden select-none">
-          <Image
-            src={user.profilePic || ""}
-            alt="user-profile"
-            width={100}
-            height={100}
-            className="object-contain"
-          />
+        <div className="md:w-[2.5vw] md:h-[2.5vw] h-[4.7vh] w-[4.7vh]  select-none cursor-pointer ">
+          <div ref={profileRef} className="">
+            <Image
+              onClick={() => setOpenPanel(!openPanel)}
+              src={user.profilePic || ""}
+              alt="user-profile"
+              width={100}
+              height={100}
+              className="object-contain rounded-full"
+            />
+          </div>
+
+          <div
+            ref={panelRef}
+            className={`hidden ${
+              openPanel ? "md:flex md:flex-col" : "md:hidden"
+            } items-center justify-center  p-[.5vw] rounded-md absolute top-[4vw] right-[10vw] bg-zinc-300 dark:bg-zinc-700 z-70 transition-all duration-300 ease-in-out w-[11vw] gap-2`}
+          >
+            {PanelLinks.map((item, index) => (
+              <div
+                key={index}
+                className="text-[1.1vw] font-prime font-[500] dark:text-zinc-200 text-zinc-600 transition ease-in-out duration-200 cursor-pointer dark:bg-zinc-600 bg-zinc-200 hover:bg-zinc-100 dark:hover:bg-zinc-500 px-[1vw] pt-[.2vw] pb-[.3vw] rounded-md w-full"
+              >
+                <Link href={item.path} className="flex items-center gap-[.7vw]">
+                  {item.icon && (
+                    <item.icon className="size-6 text-zinc-700 dark:text-zinc-200" />
+                  )}{" "}
+                  {item.name}
+                </Link>
+              </div>
+            ))}
+
+            <button
+              type="button"
+              onClick={handelLogout}
+              className="text-[1.1vw] font-prime font-[500] text-red-400 hover:text-zinc-200 transition ease-in-out duration-200 cursor-pointer dark:bg-zinc-600 bg-zinc-200 hover:bg-[#ff5855ed] px-[1vw] pt-[.2vw] pb-[.3vw] rounded-md w-full text-start flex items-center gap-[.7vw] group"
+            >
+              <IconLogout className="size-6 text-red-400 group-hover:text-zinc-200 transition ease-in-out duration-200" />{" "}
+              Logout
+            </button>
+          </div>
         </div>
       ) : (
         <div className="">
--- client/src/components/Layouts/OpenSearchSection.tsx
+++ client/src/components/Layouts/OpenSearchSection.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IconArrowNarrowLeft } from "@tabler/icons-react";
+import { IconArrowNarrowLeft, IconSearch } from "@tabler/icons-react";
 
 const OpenSearchSection = ({
   openSearch,
@@ -20,11 +20,15 @@ const OpenSearchSection = ({
                 className="md:size-[2.6vw] size-[5vh] w-[6vh] md:w-[3vw] text-zinc-700 dark:text-zinc-200 cursor-pointer hover:bg-zinc-300 dark:hover:bg-zinc-600 transition ease-in-out duration-200 rounded-full md:bg-[#f5f5f5d5] md:dark:bg-zinc-700 bg-zinc-200 dark:bg-zinc-600"
               />
 
-              <input
-                type="text"
-                placeholder="Search for articales..."
-                className="w-full placeholder:text-zinc-700 dark:placeholder:text-zinc-200 outline-none md:pl-[1vw] md:pr-[4vw] md:py-[.3vw] pl-[1.5vh] pr-[6vh] py-[.5vh] rounded-full md:rounded-xl border-2 border-zinc-400 dark:border-zinc-600 text-zinc-700 dark:text-zinc-200 bg-transparent text-[1.7vh] md:text-[1vw]"
-              />
+              <div className="relative w-full">
+                <input
+                  type="text"
+                  placeholder="Search for articales..."
+                  className="w-full placeholder:text-zinc-700 dark:placeholder:text-zinc-200 outline-none md:pl-[1vw] md:pr-[4vw] md:py-[.3vw] pl-[1.5vh] pr-[6vh] py-[.5vh] rounded-full  border-2 border-zinc-400 dark:border-zinc-600 text-zinc-700 dark:text-zinc-200 bg-transparent text-[1.7vh] md:text-[1vw]"
+                />
+
+                <IconSearch className="absolute top-[50%] right-[2vh] translate-y-[-50%] text-zinc-700 dark:text-zinc-200"/>
+              </div>
             </div>
           </div>
         </div>
