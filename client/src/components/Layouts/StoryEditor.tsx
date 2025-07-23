ab1dc62e65fde7f3810eae4a63fabe3758aa4147
kunals001 <kunaltrad555@gmail.com>
2025-07-23 21:39:29 +0530

    add tool bar

--- client/src/components/Layouts/StoryEditor.tsx
+++ client/src/components/Layouts/StoryEditor.tsx
@@ -8,6 +8,11 @@ import { HistoryPlugin } from '@lexical/react/LexicalHistoryPlugin';
 import { OnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
 import { LexicalErrorBoundary } from '@lexical/react/LexicalErrorBoundary';
 import { $getRoot } from 'lexical';
+import dynamic from 'next/dynamic';
+
+const Toolbar = dynamic(() => import('./Toolbar'), {
+  ssr: false,
+});
 
 interface Props {
   content: string;
@@ -27,13 +32,14 @@ const StoryEditor: React.FC<Props> = ({ content, setContent }) => {
 
   return (
     <LexicalComposer initialConfig={initialConfig}>
-      <div className="relative w-full min-h-[200px] border-2 border-zinc-400 dark:border-zinc-600 rounded-xl p-4 bg-zinc-200 dark:bg-zinc-800 transition-colors duration-300 mt-[2vh] ">
+      <div className="relative w-full min-h-[200px] border-2 border-zinc-400 dark:border-zinc-600 rounded-xl bg-zinc-200 px-2 dark:bg-zinc-800 transition-colors duration-300 mt-[2vh] pt-1">
+        <Toolbar />
         <RichTextPlugin
           contentEditable={
-            <ContentEditable className="min-h-[160px] w-full outline-none text-zinc-800 dark:text-zinc-100 placeholder-zinc-400" />
+            <ContentEditable className="min-h-[20vh] w-full outline-none text-zinc-800 dark:text-zinc-100 placeholder-zinc-400" />
           }
           placeholder={
-            <div className="absolute top-4 left-4 text-zinc-400 pointer-events-none select-none text-[1.6vh] md:text-[.9vw]">
+            <div className="absolute md:top-[2vw] top-[4vh] left-3 text-zinc-400 dark:text-zinc-500 pointer-events-none select-none text-[1.6vh] md:text-[.9vw]">
               Start writing your story...
             </div>
           }
---
+++ client/src/components/Layouts/Toolbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Toolbar = () => {
+  return (
+    <div className='sticky md:top-[6vw] top-[9vh] left-0 w-full md:px-[1vw] md:py-[.5vw] px-[1vh] py-[.4vh] bg-transparent z-20 rounded-xl backdrop-blur-md border-1 border-zinc-300 dark:border-zinc-700 '>
+        
+    </div>
+  )
+}
+
+export default Toolbar
