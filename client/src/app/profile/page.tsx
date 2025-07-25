077b29f841ef81548007d8d86519d4b5002a5383
kunals001 <kunaltrad555@gmail.com>
2025-07-25 15:39:31 +0530

    add profile route

---
+++ client/src/app/profile/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Skeleton from '@/components/Layouts/Skeleton'
+import dynamic from 'next/dynamic'
+
+
+const Navbar = dynamic(() => import("@/components/Navbar/Navbar"), { ssr: false,
+  loading: () => <Skeleton width={"w-full md:w-[80vw]"} height={"h-[7vh] md:h-[4vw]"} animation="shimmer" rounded='rounded-xl'/>,
+})
+
+const Profile = () => {
+  return (
+    <div className='min-h-screen w-full bg-zinc-200 dark:bg-zinc-800 flex flex-col items-center'>
+      <Navbar />
+      <div className='md:px-[10vw] px-[1vh] md:py-[1vw] py-[1vh]'>
+        <h1>Profile</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
